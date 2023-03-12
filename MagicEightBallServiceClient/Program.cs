2eb438c023a029674049aafbc35deb1734dfa55f
MustafizSaadi <mustafizur.rahman.1167141@gmail.com>
2023-03-12 11:06:12 +0600

    first commit

---
+++ MagicEightBallServiceClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicEightBallServiceClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EightBallClient eightBallClient = new EightBallClient();
+            Console.WriteLine(eightBallClient.ObtainAnswerToQuestion("What should I do now?"));
+        }
+    }
+}
---
+++ MagicEightBallServiceVSToolClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MagicEightBallServiceVSToolClient.ServiceReference1;
+
+namespace MagicEightBallServiceVSToolClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Ask the Magic 8 Ball *****\n");
+            using (EightBallClient ball = new EightBallClient("NetTcpBinding_IEightBall"))
+            {
+                //"BasicHttpBinding_IEightBall" -> This can also be used
+                Console.Write("Your question: ");
+                string question = Console.ReadLine();
+                string answer =
+                ball.ObtainAnswerToQuestion(question);
+                Console.WriteLine("8-Ball says: {0}", answer);
+            }
+            Console.ReadLine();
+        }
+    }
+}
